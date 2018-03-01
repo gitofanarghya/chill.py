3417f4d94764703f6e649e0ddee002bb62a00bab
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2018-03-01 12:27:42 +0530

    first and last commit

---
+++ chill.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+while True:
+	pyautogui.moveRel(100, 0, duration=0.25)
+	pyautogui.moveRel(0, 100, duration=0.25)
+	pyautogui.moveRel(-100, 0, duration=0.25)
+	pyautogui.moveRel(0, -100, duration=0.25)
